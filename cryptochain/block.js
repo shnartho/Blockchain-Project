6f98d829ff9c37fbcb57a4b46f27610ee1085134
shnartho <shnartho@gmail.com>
2022-02-22 19:20:17 +0100

    Setting up test driven environment

--- cryptochain/block.js
+++ cryptochain/block.js
@@ -1,20 +1,13 @@
 class Block {
-    constructor({timestamp, lastHash, hash, data}){
-        this.timestamp = timestamp;
-        this.data = data;
-        this.hash = hash;
-        this.lastHash = lastHash;
+     constructor({timestamp, lastHash, hash, data}){
+     this.timestamp = timestamp;
+     this.data = data;
+     this.hash = hash; 
+     this.lastHash = lastHash;
    
         
 
     }
 }
 
-const block1 = new Block({
-    timestamp: '01/02/2008',
-    lastHash: '0000',
-    data: 'Half fenny recieved 100 bitcoin',
-    hash: 'Willgenerate a new hash'
-    
-});
-console.log('block1', block1)
+module.exports = Block;
