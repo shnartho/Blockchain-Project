7d17ff2f7e76d765e26ddaeb3532ca1c901ff0fd
shnartho <shnartho@gmail.com>
2022-02-23 02:03:11 +0100

    genesis block

--- cryptochain/block.js
+++ cryptochain/block.js
@@ -1,13 +1,18 @@
+const { GENESIS_DATA } = require("./config");
+
 class Block {
      constructor({timestamp, lastHash, hash, data}){
      this.timestamp = timestamp;
      this.data = data;
      this.hash = hash; 
      this.lastHash = lastHash;
+     }
    
-        
+     static genesis(){
+         return new this(GENESIS_DATA);
+     }   
 
-    }
+    
 }
 
 module.exports = Block;
